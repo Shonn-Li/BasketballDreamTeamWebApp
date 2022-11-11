4bd27e01b53e8f1a1eea33d9f8f7514a66175131
Dhruv <dbhutani@uwaterloo.ca>
2022-11-11 18:32:09 -0500

    Commit from PyCharm

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+# Create your models here.
+class React(models.Model):
+    name = models.CharField(max_length=30)
+    detail = models.CharField(max_length=30)
+
+
 class PlayerInfo(models.Model):
     pid = models.CharField(max_length=150, primary_key=True)
     Name = models.CharField(max_length=150)
@@ -17,15 +23,24 @@ class PlayerInfo(models.Model):
     TOV = models.DecimalField(max_digits=10, decimal_places=2)
     TRB = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.pid
+
 
 class User(models.Model):
     user_id = models.CharField(max_length=150, primary_key=True)
     username = models.CharField(max_length=150)
     password = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.user_id
+
 
 class FavouritePlayer(models.Model):
     class Meta:
         unique_together = (('user_id', 'pid'),)
-    user_id = ForeignKey('User', on_delete=models.CASCADE, primary_key=True)
-    pid = ForeignKey('PlayerInfo', on_delete=models.CASCADE)
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE, primary_key=True)
+    pid = models.ForeignKey('PlayerInfo', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user_id

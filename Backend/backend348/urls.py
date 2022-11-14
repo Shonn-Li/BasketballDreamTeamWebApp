cb6c131148eb0212dcfd5b02d755193f1b1468f3
Dhruv <dbhutani@uwaterloo.ca>
2022-11-14 13:54:18 -0500

    Commit from PyCharm

--- Backend/backend348/urls.py
+++ Backend/backend348/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 # from rest_framework import views
 from core.views import ReactView
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 

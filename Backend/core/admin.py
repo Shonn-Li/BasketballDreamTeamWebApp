57ba5837148400fee79a8eb232ce0310c693b108
Dhruv <dbhutani@uwaterloo.ca>
2022-11-14 13:35:05 -0500

    Commit from PyCharm

--- Backend/core/admin.py
+++ Backend/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import PlayerInfo, FavouritePlayer, User
+
+admin.site.register(PlayerInfo)
+admin.site.register(User)
+admin.site.register(FavouritePlayer)
+
 # Register your models here.
--- Backend/core/views.py
+++ Backend/core/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from .models import *
 from rest_framework.response import Response
 from .serializer import *
 
 # Create your views here.
+
+
+class PlayerInfoViewSet(viewsets.ModelViewSet):
+    queryset = PlayerInfo.objects.all().order_by('pid')
+    serializer_class = PlayerInfoSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = PlayerInfo.objects.all().order_by('user_id')
+    serializer_class = UserSerializer
+
+
+class FavouritePlayerViewSet(viewsets.ModelViewSet):
+    queryset = PlayerInfo.objects.all().order_by('user_id')
+    serializer_class = FavouritePlayerSerializer
+
 
 class ReactView(APIView):
 

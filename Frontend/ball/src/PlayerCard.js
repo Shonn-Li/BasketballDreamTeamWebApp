7cd339b0865eac7476453757832bc1e2d3f60f46
Shonn Li <77355994+Shonn-Li@users.noreply.github.com>
2022-11-17 01:18:07 -0500

    working stuff

--- Frontend/ball/src/PlayerCard.js
+++ Frontend/ball/src/PlayerCard.js
@@ -1,43 +1,93 @@
 import * as React from 'react';
 import './PlayerCard.scss';
+import axios from 'axios';
 
 function PlayerCard() {
+  // return (
+  //   <div className="wrapper">
+  //     <Card
+  //       img=""
+  //       name=""
+  //       height=""
+  //       team=""
+  //     />
+
+  //       <Card
+  //       img=""
+  //       name=""
+  //       height=""
+  //       team=""
+  //     />
+
+  //       <Card
+  //       img=""
+  //       name=""
+  //       height=""
+  //       team=""
+  //     />
+
+  //       <Card
+  //       img=""
+  //       name=""
+  //       height=""
+  //       team=""
+  //     />
+
+  //       <Card
+  //       img=""
+  //       name=""
+  //       height=""
+  //       team=""
+  //     />
+  //   </div>
+  // );
+
+  const [state, setState] = React.useState(null);
+
+  React.useEffect(() => {
+    console.log("testing");
+    getPlayers();
+  })
+
+  function getPlayers() {
+    axios({
+      method:"GET",
+      url: "http://localhost:8000/api/"
+    }).then((res) => {
+      const data = res.data;
+      console.log(data);
+      setState(data);
+      console.log(state);
+    }).catch((error) => {
+      if(error.message) {
+        console.log(error.response);
+      }
+    })
+  }
+
+  if(!state) return null;
+
   return (
-    <div className="wrapper">
-      <Card
-        img=""
-        name=""
-        height=""
-        team=""
-      />
-
-        <Card
-        img=""
-        name=""
-        height=""
-        team=""
-      />
-
-        <Card
-        img=""
-        name=""
-        height=""
-        team=""
-      />
-
-        <Card
-        img=""
-        name=""
-        height=""
-        team=""
-      />
-
-        <Card
-        img=""
-        name=""
-        height=""
-        team=""
-      />
+    <div className='wrapper'>
+      {/* {state.map((player) => {
+        // <Card
+        //     img=""
+        //     name={player.Name}
+        //     height={player.Height_cm}
+        //     team={player.Tm}
+        // />
+        <p>
+          {player.Name}
+        </p>
+      })} */}
+        {state.map((player) => {
+          return <Card
+          img=""
+          name={player.Name}
+          height={player.Height_cm}
+          team={player.Tm}
+           />
+        })}
     </div>
   );
 }

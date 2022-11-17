f9946f1c5b7aba7997ae2979d8ee2007ce343118
TarunVelicheti <tarun.velicheti119@gmail.com>
2022-11-17 17:35:49 -0500

    Search API prebuild

--- Backend/backend_api/urls.py
+++ Backend/backend_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PlayerList, PlayerDetails
+from .views import PlayerList, PlayerDetails, PlayerSearch
 
 appname = 'backend_api'
 
 urlpatterns = [
     path('', PlayerList.as_view(), name='listcreate'),
-    path('<int:pk>/', PlayerDetails.as_view(), name='detailscreate')
+    path('<int:pk>/', PlayerDetails.as_view(), name='detailscreate'),
+    path('search', PlayerSearch.as_view(), name='searchplayers')
 ]
--- Backend/backend_api/views.py
+++ Backend/backend_api/views.py
@@ -9,10 +9,15 @@ from .serializer import PlayerInfoSerializer
 class PlayerList(generics.ListCreateAPIView):
     queryset = PlayerInfo.objects.all()
     serializer_class = PlayerInfoSerializer
-    pass
 
 class PlayerDetails(generics.RetrieveDestroyAPIView):
     queryset = PlayerInfo.objects.all()
     serializer_class = PlayerInfoSerializer
+
+class PlayerSearch(generics.ListAPIView):
+    queryset = PlayerInfo.objects.all()
+    serializer_class = PlayerInfoSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^Name']
 
 

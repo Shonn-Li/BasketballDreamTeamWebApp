39637b0784984f0683fc03e76643aa78eb1c19d9
TarunVelicheti <tarun.velicheti119@gmail.com>
2022-11-28 14:08:32 -0500

    Front-end changes

--- Frontend/ball/src/Intro.js
+++ Frontend/ball/src/Intro.js
@@ -1,31 +1,35 @@
-import { Box, Button, Grid, makeStyles } from '@material-ui/core';
-import { mergeClasses } from '@material-ui/styles';
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import './css/Intro.css'
+import { Box, Button, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+// import { mergeClasses } from '@material-ui/styles';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./css/Intro.css";
 
 const useStyles = makeStyles({
-    root: {
-        backgrond: 'linear-gradient(45deg, #FE6B8B #FF8E53)',
-        border: 0, 
-        marginBottom: 1,
-        borderRadius: 15,
-        color: 'white',
-        padding: '15px 30px'
-    }
-})
+ root: {
+  backgrond: "linear-gradient(45deg, #FE6B8B #FF8E53)",
+  border: 0,
+  marginBottom: 1,
+  borderRadius: 15,
+  color: "white",
+  padding: "15px 30px",
+ },
+});
 
 function Intro() {
-    let history = useNavigate();
-    return (
-        <header className="main-container">
-                <Button variant="contained" onClick={() => {
-                    history("/Login")
-                }}>
-                    Build Your Team Now!
-                </Button>
-        </header>
-    );
+ let history = useNavigate();
+ return (
+  <header className="main-container">
+   <Button
+    variant="contained"
+    onClick={() => {
+     history("/Login");
+    }}
+   >
+    Build Your Team Now!
+   </Button>
+  </header>
+ );
 }
 
 export default Intro;
